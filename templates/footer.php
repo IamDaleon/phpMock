a4461bfc3c0d26e2199b8f9107806c4836266def
Daleon L <daleon86@gmail.com>
2020-04-11 23:40:33 -0500

    Minor changes

---
+++ templates/footer.php
@@ -0,0 +1,40 @@
+
+<div>
+  <div class="container text-center">
+  <div class="container">
+  <div class="row justify-content-md-center">
+    <div class="col col-lg-2">
+      <a href="http://"> FAQ's</a>
+    </div>
+    <div class="col-md-auto">
+    <a href="http://">Careers</a>
+    </div>
+    <div class="col col-lg-2">
+     <a href="http://">
+          Gift Cards
+     </a>
+    </div>
+    <div class="col col-lg-2">
+     <a href="http://">
+          Community
+     </a>
+    </div>
+  </div>
+  </div>
+  <div class="row text-center">
+    <div class="col">
+      About
+    </div>
+    <div class="col">
+      Connect with us
+    </div>
+    <div class="col">
+          Partners
+    </div>
+  </div>
+     <div>
+          <h5 class="text-center">
+               &copy; 2020 - <?php echo date("D M d,Y");?>
+          </h5>
+     </div>
+</div>
---
+++ templates/navBar.php
@@ -0,0 +1,35 @@
+<!-- Image and text -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <a class="navbar-brand" href="#">
+        <img src="/docs/4.4/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        CarreraRoberts
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="container">
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                About
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Menu</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Special</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+            </ul>
+        </div>
+    </div>
+</nav>
